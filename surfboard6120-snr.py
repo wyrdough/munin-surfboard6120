2bd449769aa215349da8d58f3925149d5d3aa491
Jeffrey Forman <jeff@jeffreyforman.net>
2010-10-16 16:42:11 -0400

    Actually works now, iterates over channels and SNR values. Output what munin expects

--- surfboard6120-snr.py
+++ surfboard6120-snr.py
@@ -2,12 +2,35 @@
 
 import urllib
 import re
+import sys
+
+config_text = """graph_title Signal 
+graph_vlabel dB (decibels)"""
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "config":
+        print config_text
+        sys.exit(0)
+
 
 cmOutput = urllib.urlopen("http://192.168.100.1/cmSignalData.htm").read()
 cmOutput = re.sub('\n','', cmOutput)
 cmOutput = re.sub('&nbsp;', '', cmOutput)
+cmOutput = re.sub(r'<.*?>', '', cmOutput)
 
-snrOutput = re.search(r"<TR><TD>Signal to Noise Ratio</TD>.*dB</TD></TR>", cmOutput).group(0)
+dOutput = re.search(r"Downstream(.*)Upstream", cmOutput).group(1)
 
+snrOutput = re.search(r"Signal to Noise Ratio(.*dB).*Downstream", dOutput).group(1)
+
+counter = 0
+# Iterate over SNR Values
 for current in re.finditer(r"\d+", snrOutput):
-    print current.group(0)
+    print "snr%d.value %s" % (counter, current.group(0))
+    counter = counter + 1
+
+# Iterate over Downstream Channels
+smChannels = re.search(r"Channel ID([0-9\s]+)", dOutput).group(1)
+counter = 0
+for current in re.finditer(r"\d+\s", smChannels):
+    print "channel%d.value %s" % (counter, current.group(0))
+    counter = counter + 1
